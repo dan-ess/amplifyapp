a45af2f64f3153a268ea51d170b437d2ab37da0b
Dan Schmierer <dan-ess@users.noreply.github.com>
2022-02-24 01:50:34 +0900

    added auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
-export default App;
+
+// import { Authenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';
+
+// import awsExports from './aws-exports';
+// import Amplify from 'aws-amplify';
+// Amplify.configure(awsExports);
+
+// function App() {
+//   return (
+//     <Authenticator>
+//       {({signOut, user}) => (
+//         <div className='App'>
+//           <p>
+//             Hey {user.username}, welcome to my channel, with auth!
+//           </p>
+//           <button onClick={signOut}>Sign out</button>
+//         </div>
+//       )};
+//       <div>hi</div>
+//     </Authenticator>
+
+//   );
+// }
+
+// export default App;
